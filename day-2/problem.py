252fae74c2d7cfdf1093e5e1fbc2c7fb320a0191
joshcarp <joshuacarpeggiani@gmail.com>
2021-12-05 18:15:42 +1100

    Update

--- day-2/problem.py
+++ day-2/problem.py
@@ -2,14 +2,15 @@ if __name__ == "__main__":
     datain = open("input")
     x, y = 0, 0
     for i, e in enumerate(datain):
-        direction = e.split()
-        print(direction[0])
-        if direction[0] == "forward":
-            x += int(direction[1])
-        if direction[0] == "backward":
-            x -= int(direction[1])
-        if direction[0] == "up":
-            y -= int(direction[1])
-        if direction[0] == "down":
-            y += int(direction[1])
+        all = e.split()
+        direction = all[0]
+        distance = int(all[1])
+        if direction == "forward":
+            x += distance
+        if direction == "backward":
+            x -= distance
+        if direction == "up":
+            y -= distance
+        if direction == "down":
+            y += distance
     print(x*y)
